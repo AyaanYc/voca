d582b76b42fa63f80a906843d76b7f5b170c21eb
YE CHAN <qjdrptmzk@naver.com>
2022-08-23 13:19:00 +0900

    react exam7

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Exam1 from './Exam1';
-import Exam2 from './Exam2';
-import Exam3 from './Exam3';
-import Exam4 from './Exam4';
-import Exam5 from './Exam5';
+import Exam1 from './Exam/Exam1';
+import Exam2 from './Exam/Exam2';
+import Exam3 from './Exam/Exam3';
+import Exam4 from './Exam/Exam4';
+import Exam5 from './Exam/Exam5';
+import Exam6 from './Exam/Exam6';
+import Exam7 from './Exam/Exam7';
 
 function App() {
   return (
     <div className="App">
-      <Exam1/>      
+      {/* <Exam1/>      
       <Exam2/>      
       <Exam3/>      
       <Exam4/>      
       <Exam5/>      
+      <Exam6/>       */}
+      <Exam7/>      
     </div>
   );
 }
---
+++ src/Exam/Exam7.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function TodoApp() {
+    const [ todo, setTodo ] = useState('');
+    const [ todoList, setTodoList ] = useState([]);
+
+    const onChange = (e) => {
+        setTodo(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setTodo(preVal => preVal = '');
+        if(`${todo}`== ''){
+            return;
+        } else {
+            setTodoList(preVal => preVal=`${todo}`);
+        }
+
+    }
+    return (
+        <div>
+            <h1>My Todo List</h1>
+            <form onSubmit={ onSubmit }>
+                <input
+                    type="text"
+                    placeholder="Write your to do..."
+                    value={todo}
+                    onChange={onChange}
+                />
+                <button onClick={onSubmit}>Add To do</button>
+            </form>
+        </div>
+    );
+}
